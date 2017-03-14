d947888bbdf5f6d1435b2fb74f49f6b11c339905
Kelly Kearney <kakearney@gmail.com>
2017-03-13 17:24:07 -0700

    Rewrote for more flexible input
    Rewrote function to allow x- and y-coordinate arrays to either
    match the size of z (as with pcolor) or to be one element longer.
    I also now support matrix input for x and y, and allow an axis
    handle to be passed in.

--- pcolorpad.m
+++ pcolorpad.m
@@ -1,40 +1,119 @@
-function h = pcolorpad(x,y,z, varargin)
+function h = pcolorpad(varargin)
 %PCOLORPAD Pseudocolor plot defined by edges of cells
 %
-% h = pcolorpad(x,y,z)
+% h = pcolorpad(c)
+% h = pcolorpad(x,y,c)
+% h = pcolorpad(ax, ...)
 % 
 % This function is nearly identical to pcolor, except it doesn't drop the
 % last row and column of an array.
 %
 % Input variables:
 %
-%   x:  n x 1 array defining edges in x direction
+%   c:  m x n array of color values to be plotted as a surface
 %
-%   y:  m x 1 array defining edges in y direction
+%   x:  x-coordinate data for the surface, defining the x-coordinates of
+%       the surface cell vertices.  Can be either an 1 x n vector, 1 x n+1
+%       vector, m x n array, m x n+1 array, m+1 x n array, or m+1 x n+1
+%       array.  If the number of columns in x is one more than the number
+%       of columns in c, then the last column of the c array will be
+%       visible.  If x is a 2D array, its size must match that of y.
 %
-%   z:  m-1 x n-1 array of z values
+%   y:  y-coordinate data for the surface, defining the y-coordinates of
+%       the surface cell vertices.  Can be either an m x 1 vector, m+1 x 1
+%       vector, m x n array, m x n+1 array, m+1 x n array, or m+1 x n+1
+%       array.  If the number of rows in y is one more than the number
+%       of rows in c, then the last row of the c array will be
+%       visible.  If y is a 2D array, its size must match that of x.
+%  
+%   ax: handle to axis where pcolor surface is plotted
+%
+% Optional input variables:
+%
+%   all parameter/value pairs available for a pcolor or surf plot can be
+%   passed to this function.  
+%
+% Output variables:
+%
+%   h:  handle to plotted surface object
 
-if nargin == 1
-    z = x;
-    x = 1:(size(z,2)+1);
-    y = 1:(size(z,1)+1);
-end
+% Copyright 2017 Kelly Kearney
 
-if isequal(size(z), [length(y) length(x)])
-    x2 = [x(:); NaN];
-    y2 = [y(:); NaN];
-elseif isequal(size(z), [length(y)-1 length(x)-1])
-    x2 = x;
-    y2 = y;
-elseif isequal(size(x), size(y), size(z)+[1 1])
-    x2 = x;
-    y2 = y;
+% Look for axis input
+
+isax = cellfun(@(x) isscalar(x) && ishandle(x) && strcmp(get(x, 'type'), 'axes'), varargin);
+if any(isax)
+    ax = varargin{isax};
+    args = varargin(~isax);
 else
-    error('z must be length(y) x length(x) or length(y)-1 x length(x)-1 array');
+    args = varargin;
+    ax = gca;
 end
 
-[nrow, ncol] = size(z);
-z2 = nan(nrow+1, ncol+1);
-z2(1:nrow, 1:ncol) = z;
+% Look for surf/pcolor parameter-value pairs
 
-h = pcolor(x2, y2, z2, varargin{:});
+isstr = cellfun(@ischar, args);
+if any(isstr)
+    idx = find(isstr,1);
+    params = args(idx:end);
+    xyz = args(1:idx-1);
+else
+    xyz = args;
+    params = {};
+end
+
+% Check input
+
+if length(xyz) == 1
+    z = xyz{1};
+    validateattributes(z, {'numeric'}, {'2d'}, 'pcolorpad', 'c');
+    
+    x = 1:size(z,2)+1;
+    y = 1:size(z,1)+1;
+    
+elseif length(xyz) == 3
+    x = xyz{1};
+    y = xyz{2};
+    z = xyz{3};
+    
+    validateattributes(x, {'numeric'}, {'2d'}, 'pcolorpad', 'x');
+    validateattributes(y, {'numeric'}, {'2d'}, 'pcolorpad', 'y');
+    validateattributes(z, {'numeric'}, {'2d'}, 'pcolorpad', 'c');
+    
+else
+    error('Incorrect number of input arguments');
+end
+
+% Check sizes of x and y to determine where to pad
+
+[ny, nx] = size(z);
+
+if isvector(x)
+    x = reshape(x, 1, []);
+end
+if isvector(y)
+    y = y(:);
+end
+
+if ~ismember(size(x,2), [nx nx+1])
+    error('Number of columns in x array must match size(c,2) or size(c,2)+1');
+end
+if ~ismember(size(y,1), [ny ny+1])
+    error('Number of rows in y array must match size(c,1) or size(c,1)+1');
+end
+
+% Pad x
+
+if size(x,2) == nx+1
+    z = [z nan(ny,1)];
+end 
+
+% Pad y
+
+if size(y,1) == ny+1
+    z = [z; nan(1, size(z,2))];
+end
+
+% Plot
+    
+h = pcolor(ax, x, y, z, params{:});
